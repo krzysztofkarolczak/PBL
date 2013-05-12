1d729b60e5d1de041a74c44f6ef5e5e9eb46f6b4
Bartosz <janel5@live.com>
2013-05-12 10:03:59 +0200

    [+] trajectory calculation

--- KinectPackage/KinectRecorder/KinectProjection/MainWindow.xaml.cs
+++ KinectPackage/KinectRecorder/KinectProjection/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ using KinectLib;
 using Microsoft.Win32;
 using System.Timers;
 using System.Windows.Threading;
+using MathNet.Numerics;
 
 namespace KinectProjection
 {
@@ -35,9 +36,11 @@ namespace KinectProjection
         double c_max;
         double c_min;
         double distance_to_start = 2.5;
+        double a, b,slope;
         List<int> legs_int = new List<int>();
         List<int> leg_right = new List<int>();
         List<int> leg_left = new List<int>();
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -50,6 +53,7 @@ namespace KinectProjection
             timer.Interval = TimeSpan.FromMilliseconds(33);
             timer.Tick += timer_Tick;
             timer.Start();
+            
             
         }
 
@@ -64,6 +68,7 @@ namespace KinectProjection
                 double y = frame.Y - frame_old.Y;
 
                 canvas1.Children.Clear();
+                canvas2.Children.Clear();
                 Ellipse el = new Ellipse();
                 el.Width = 10;
                 el.Height = 10;
@@ -77,46 +82,98 @@ namespace KinectProjection
                 var hip = frame.Nodes.FirstOrDefault(o=>o.NodeType==center);
 
 
-                frame.Nodes.Where(o=>legs_int.Contains(o.NodeType)).Zip(frame_old.Nodes.Where(o=>legs_int.Contains(o.NodeType)), (e1, e2) =>
+                //frame.Nodes.Where(o=>legs_int.Contains(o.NodeType)).Zip(frame_old.Nodes.Where(o=>legs_int.Contains(o.NodeType)), (e1, e2) =>
+                //{
+
+                //    SkeletonNode n = new SkeletonNode();
+
+                //    double deg = Math.PI / 2;
+
+                //    n.X = (e1.X + e1.Z * Math.Tan(deg)) / Math.Sqrt(1 + Math.Tan(deg) * Math.Tan(deg));
+                //    n.Y = e1.Y ;
+
+
+                //    Point a = new Point();
+                //    a.X = e1.X - x;
+                //    a.Y = e1.Y - y;
+                //    //a.X = e1.X - x - hip.X;
+                //    //a.Y = e1.Y - y - hip.Y;
+                //    //rotation
+
+                //    a.X = n.X - x;
+                //    a.Y = n.Y - y;
+
+                //    SolidColorBrush b = new SolidColorBrush(Colors.Black);
+                //    if(leg_right.Contains(e1.NodeType))
+                //        b = new SolidColorBrush(Colors.Red);
+                //    else
+                //        b = new SolidColorBrush(Colors.Blue);
+                //    return new { X=a.X,Y=a.Y,br=b };
+                //}).ToList().ForEach(o =>
+                //{
+                //    el = new Ellipse();
+                //    el.Width = 10;
+                //    el.Height = 10;
+                //    el.Fill = o.br;
+                //    Canvas.SetLeft(el, off_x +  o.X * 100);
+                //    Canvas.SetTop(el, off_y -  o.Y * 100);
+                //    canvas1.Children.Add(el);
+                    
+                //});
+
+
+                frame.Nodes.Where(o => legs_int.Contains(o.NodeType)).ToList().ForEach(o =>
                 {
+                    SkeletonNode n = new SkeletonNode();
+                    SkeletonNode nhip = new SkeletonNode();
 
-                    //SkeletonNode n = new SkeletonNode();
+                    double deg =  Math.PI / 2;
 
-                    //double deg = Math.PI / 2;
+                    n.X = (o.X + o.Z * Math.Tan(deg)) / Math.Sqrt(1 + Math.Tan(deg) * Math.Tan(deg));
+                    n.Y = o.Y;
 
-                    //n.Y = e1.Y * Math.Cos(deg) - e1.Z * Math.Sin(deg);
-                    //n.Z = e1.Y * Math.Sin(deg) + e1.Z * Math.Cos(deg);
+
+                    nhip.X = (hip.X + hip.Z * Math.Tan(deg)) / Math.Sqrt(1 + Math.Tan(deg) * Math.Tan(deg));
+                    nhip.Y = hip.Y;
 
 
                     Point a = new Point();
-                    a.X = e1.X - x;
-                    a.Y = e1.Y - y;
-                    //a.X = e1.X - x - hip.X;
-                    //a.Y = e1.Y - y - hip.Y;
+                    Point a1 = new Point();
+                    a.X = o.X - x;
+                    a.Y = o.Y - y;
+                    a.X = o.X  - hip.X;
+                    a.Y = o.Y  - hip.Y;
                     //rotation
 
-                    //a.X = n.X - x;
-                    //a.Y = n.Y - y;
+
+                    a1.X = n.X  -nhip.X;
+                    a1.Y = n.Y  - nhip.Y;
 
                     SolidColorBrush b = new SolidColorBrush(Colors.Black);
-                    if(leg_right.Contains(e1.NodeType))
+                   
+                    if (leg_right.Contains(o.NodeType))
                         b = new SolidColorBrush(Colors.Red);
                     else
                         b = new SolidColorBrush(Colors.Blue);
-                    return new { X=a.X,Y=a.Y,br=b };
-                }).ToList().ForEach(o =>
-                {
+
                     el = new Ellipse();
                     el.Width = 10;
                     el.Height = 10;
-                    el.Fill = o.br;
-                    Canvas.SetLeft(el, off_x +  o.X * 100);
-                    Canvas.SetTop(el, off_y -  o.Y * 100);
+                    el.Fill = b;
+                    Canvas.SetLeft(el, off_x + a.X * 100);
+                    Canvas.SetTop(el, off_y - a.Y * 100);
                     canvas1.Children.Add(el);
-                    
+
+                    el = new Ellipse();
+                    el.Width = 10;
+                    el.Height = 10;
+                    el.Fill = b;
+                    Canvas.SetLeft(el, off_x + a1.X * 100);
+                    Canvas.SetTop(el, off_y - a1.Y * 100);
+                    canvas2.Children.Add(el);
+
                 });
 
-                
                 
                 
                 label1.Content = index++;
@@ -134,7 +191,37 @@ namespace KinectProjection
                 OpenedFrames = formatter.Deserialize(writer) as List<KinectLib.SkeletonFrame>;
             }
 
-            int b = 1;
+            List<Point> points = new List<Point>();
+
+            foreach (var frame in OpenedFrames)
+            {
+                Point p = new Point();
+                p.Y = frame.X;
+                p.X = frame.Z;
+                points.Add(p);
+            }
+
+            Matrix A = new Matrix();
+            Vector X = new Vector();
+            Vector B = new Vector();
+
+            A.M11 = points.Count();
+            A.M12 = points.Sum(o=> o.X);
+            A.M21 = points.Sum(o => o.X);
+            A.M22 = points.Sum(o => o.X * o.X);
+
+            B.X = points.Sum(o => o.Y);
+            B.Y = points.Sum(o => o.X * o.Y);
+
+            A.Invert();
+
+            X.X = A.M11 * B.X + A.M12 * B.Y;
+            X.Y = A.M21 * B.X + A.M22 * B.Y;
+
+            b = X.X;
+            a = X.Y;
+            slope = Math.Atan(a);
+                        
             off_x = canvas1.Width / 2;
             off_y = canvas1.Height / 2;
             canvas1.Children.Clear();
